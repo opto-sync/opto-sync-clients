ba636209e851d44bd2d4ff8e59c04fb0fc0095c9
Alexander Mills <alex@oresoftware.com>
2026-07-30 12:49:43 -0500

    Format the RxDart background runner

--- clients/reactive-dart/lib/src/background_sync.dart
+++ clients/reactive-dart/lib/src/background_sync.dart
@@ -82,12 +82,12 @@ ValueStream<BackgroundSyncOutcome<R>> createBackgroundSyncOutcomes<R>({
       .exhaustMap(
         (wake) => Stream<BackgroundSyncOutcome<R>>.fromFuture(
           runner.runOnce().then(
-            (result) => BackgroundSyncOutcome<R>(
-              wake: wake,
-              ok: true,
-              result: result,
-            ),
-          ),
+                (result) => BackgroundSyncOutcome<R>(
+                  wake: wake,
+                  ok: true,
+                  result: result,
+                ),
+              ),
         ).onErrorReturnWith(
           (error, stackTrace) => BackgroundSyncOutcome<R>(
             wake: wake,
