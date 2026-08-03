d80a519895d5418064ada5261ea85735aa1e0c3a
Alexander Mills <alex@oresoftware.com>
2026-08-03 12:45:02 -0500

    DEN-1538 add Dart capability report

---
+++ formal/sdk/dart/tool/report_capabilities.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+import 'package:fm_adapter_stream/fm_adapter_capabilities.dart';
+
+void main() {
+  stdout.writeln('protocol\t$streamAdapterProtocol');
+  stdout.writeln('protocolVersion\t$streamAdapterProtocolVersion');
+  stdout.writeln('registry\t${capabilityArrayJsonV1(capabilityRegistryV1())}');
+  stdout.writeln('required\t${capabilityArrayJsonV1(requiredCapabilitiesV1())}');
+  for (final List<String> sequence in allCanonicalCapabilitySequencesV1()) {
+    stdout.writeln('sequence\t${capabilityArrayJsonV1(sequence)}');
+  }
+}
